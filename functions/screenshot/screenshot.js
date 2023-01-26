c31a552971a45a3caa001f717e4730b0033591ef
Nick De Frangh <nick@plutus.rocks>
2023-01-26 23:30:47 +0100

    logging

--- functions/screenshot/screenshot.js
+++ functions/screenshot/screenshot.js
@@ -10,25 +10,32 @@ exports.handler = async (event, context) => {
   //   };
   const pageToScreenshot = "https://store.epicgames.com/en-US/";
 
+  console.log("Launching chromium");
   browser = await playwright.launchChromium();
   const browserContext = await browser.newContext();
   const page = await browserContext.newPage();
 
+  console.log("opening page");
   await page.goto(pageToScreenshot, {
     waitUntil: "networkidle",
     timeout: 60 * 1000,
   });
 
+  console.log("Fetching block");
   const block = await page
     .locator("h2", { hasText: "Free Games" })
     .locator("..")
     .locator("..");
   await block.scrollIntoViewIfNeeded();
   await page.waitForLoadState("networkidle");
+
+  console.log("Taking screenshot");
   const buffer = await block.screenshot({ path: "screenshots/screenshot.png" });
 
+  console.log("cllsing browser");
   await browser.close();
 
+  console.log("Returning result");
   return {
     statusCode: 200,
     headers: {
